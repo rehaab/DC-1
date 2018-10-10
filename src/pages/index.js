d8dc81a5d325cda46f329bf8ff562f04630ab79c
Rehab Seyani <rehabseyani@Rehaabs-Macbook.local>
2018-10-10 15:13:29 +1100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to <br />design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. It's finally time to learn React and Swift Rehaabi my love. No more excuses. </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
